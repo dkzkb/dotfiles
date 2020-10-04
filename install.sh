ba85963e6df0f73937ea767b5ccd40c69e76586f
dkzkb <kazu@kubo.asia>
2020-10-04 18:10:05 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ fi
 
 cd "$DOTFILES_DIR"
 
+test -e "$HOME/.aliases" && mv "$HOME/.aliases" "$HOME/.aliases.bak"
+ln -fsv "$DOTFILES_DIR/src/.aliases" "$HOME/.aliases"
+
 test -e "$HOME/.bashrc" || mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
 ln -fsv "$DOTFILES_DIR/src/.bashrc" "$HOME/.bashrc"
-
-# test -e "$HOME/.aliases" && mv "$HOME/.aliases" "$HOME/.aliases.bak"
-# ln -fsv "$DOTFILES_DIR/src/.aliases" "$HOME/.aliases"
